f4817c4f2a359a39a0684eb7a0f474951d197849
DILPREET1910 <dsahney1910@gmail.com>
2023-09-29 16:14:24 +0530

    leaderboard after last question

--- lib/pages/win.dart
+++ lib/pages/win.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // lib imports
 import 'package:hangman_ieee_intromeet_2023/services/firestore.dart';
+import 'package:hangman_ieee_intromeet_2023/pages/leaderboard.dart';
 import 'package:hangman_ieee_intromeet_2023/widgets/hangmanTitle.dart';
 import 'package:hangman_ieee_intromeet_2023/widgets/noGlow.dart';
 import 'package:hangman_ieee_intromeet_2023/widgets/forWinLose/nextOrWaiting.dart';
@@ -31,99 +32,103 @@ class _WinState extends State<Win> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: firestore.questionStatus(widget.index + 2),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Stack(
-            children: [
-              // START: background image in stack
-              Image.asset(
-                'assets/background.png',
-                fit: BoxFit.fill,
-                width: double.infinity,
-                height: double.infinity,
-              ),
-              // END: background image in stack
-              SafeArea(
-                child: Scaffold(
-                  backgroundColor: Colors.transparent,
-                  body: RefreshIndicator(
-                    onRefresh: () async {
-                      setState(() {});
-                    },
-                    child: ScrollConfiguration(
-                      behavior: NoGlow(),
-                      child: SingleChildScrollView(
-                        physics: const AlwaysScrollableScrollPhysics(),
-                        child: SizedBox(
-                          width: double.infinity,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              // START: hangman title
-                              SizedBox(height: MediaQuery.of(context).size.height / 13),
-                              const WidgetsHangmanTitle(),
-                              // END: hangman title
-                              // START: You won text
-                              SizedBox(height: MediaQuery.of(context).size.height / 20),
-                              Text(
-                                "You Won",
-                                style: GoogleFonts.ubuntu(
+    if (widget.index < 11) {
+      return FutureBuilder(
+        future: firestore.questionStatus(widget.index + 2),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Stack(
+              children: [
+                // START: background image in stack
+                Image.asset(
+                  'assets/background.png',
+                  fit: BoxFit.fill,
+                  width: double.infinity,
+                  height: double.infinity,
+                ),
+                // END: background image in stack
+                SafeArea(
+                  child: Scaffold(
+                    backgroundColor: Colors.transparent,
+                    body: RefreshIndicator(
+                      onRefresh: () async {
+                        setState(() {});
+                      },
+                      child: ScrollConfiguration(
+                        behavior: NoGlow(),
+                        child: SingleChildScrollView(
+                          physics: const AlwaysScrollableScrollPhysics(),
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                // START: hangman title
+                                SizedBox(height: MediaQuery.of(context).size.height / 13),
+                                const WidgetsHangmanTitle(),
+                                // END: hangman title
+                                // START: You won text
+                                SizedBox(height: MediaQuery.of(context).size.height / 20),
+                                Text(
+                                  "You Won",
+                                  style: GoogleFonts.ubuntu(
+                                    color: Colors.green,
+                                    fontSize: MediaQuery.of(context).size.height / 20,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                // END: You won text
+                                // START: The right answer was text
+                                SizedBox(height: MediaQuery.of(context).size.height / 100),
+                                Text(
+                                  "The right answer was: ",
+                                  style: GoogleFonts.ubuntu(
+                                    color: Colors.green,
+                                    fontSize: MediaQuery.of(context).size.height / 30,
+                                    fontWeight: FontWeight.w300,
+                                  ),
+                                ),
+                                // END: The right answer was text
+                                // START: Answer
+                                SizedBox(height: MediaQuery.of(context).size.height / 100),
+                                WidgetsAnswerWinLose(
+                                  index: widget.index,
                                   color: Colors.green,
-                                  fontSize: MediaQuery.of(context).size.height / 20,
-                                  fontWeight: FontWeight.w500,
                                 ),
-                              ),
-                              // END: You won text
-                              // START: The right answer was text
-                              SizedBox(height: MediaQuery.of(context).size.height / 100),
-                              Text(
-                                "The right answer was: ",
-                                style: GoogleFonts.ubuntu(
-                                  color: Colors.green,
-                                  fontSize: MediaQuery.of(context).size.height / 30,
-                                  fontWeight: FontWeight.w300,
+                                // END: Answer
+                                // START: Win animation
+                                SizedBox(height: MediaQuery.of(context).size.height / 20),
+                                Container(
+                                  width: MediaQuery.of(context).size.height / 2.2,
+                                  height: MediaQuery.of(context).size.height / 3.5,
+                                  // decoration: BoxDecoration(border: Border.all(color: Colors.white)),
+                                  child: const RiveAnimation.asset(
+                                    'assets/rive/win.riv',
+                                    fit: BoxFit.fitWidth,
+                                  ),
                                 ),
-                              ),
-                              // END: The right answer was text
-                              // START: Answer
-                              SizedBox(height: MediaQuery.of(context).size.height / 100),
-                              WidgetsAnswerWinLose(
-                                index: widget.index,
-                                color: Colors.green,
-                              ),
-                              // END: Answer
-                              // START: Win animation
-                              SizedBox(height: MediaQuery.of(context).size.height / 20),
-                              Container(
-                                width: MediaQuery.of(context).size.height / 2.2,
-                                height: MediaQuery.of(context).size.height / 3.5,
-                                // decoration: BoxDecoration(border: Border.all(color: Colors.white)),
-                                child: const RiveAnimation.asset(
-                                  'assets/rive/win.riv',
-                                  fit: BoxFit.fitWidth,
-                                ),
-                              ),
-                              // END: Win animation
-                              // START: play or wait for admin
-                              SizedBox(height: MediaQuery.of(context).size.height / 20),
-                              WidgetsNextWait(start: snapshot.data!),
-                              // END: play or wait for admin
-                            ],
+                                // END: Win animation
+                                // START: play or wait for admin
+                                SizedBox(height: MediaQuery.of(context).size.height / 20),
+                                WidgetsNextWait(start: snapshot.data!),
+                                // END: play or wait for admin
+                              ],
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
-          );
-        } else {
-          return SpinKitCircle(color: Colors.green[900]);
-        }
-      },
-    );
+                )
+              ],
+            );
+          } else {
+            return const LeaderBoard();
+          }
+        },
+      );
+    } else {
+      return SpinKitCircle(color: Colors.grey[900]);
+    }
   }
 }
