7b5abc05f65665a621fb92483f9000a505674229
DILPREET1910 <dsahney1910@gmail.com>
2023-09-29 12:17:25 +0530

    all questions

--- lib/globalVariables.dart
+++ lib/globalVariables.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// google fonts imports
+import 'package:google_fonts/google_fonts.dart';
+
 String team = "";
 
 List<Widget> questions(context) {
@@ -40,6 +43,118 @@ List<Widget> questions(context) {
         'assets/ui/amazon.jpg',
       ),
     ),
+    Container(
+      padding:
+          EdgeInsets.symmetric(vertical: 0, horizontal: MediaQuery.of(context).size.width / 90),
+      child: Text(
+        "Where you’ll find people chasing imaginary creatures with more enthusiasm than they chase their life goals",
+        textAlign: TextAlign.center,
+        style: GoogleFonts.ubuntu(
+          color: Colors.white,
+          fontSize: MediaQuery.of(context).size.height / 40,
+          letterSpacing: 0,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    ),
+    Container(
+      padding:
+          EdgeInsets.symmetric(vertical: 0, horizontal: MediaQuery.of(context).size.width / 90),
+      child: Text(
+        "The digital vortex where pants are optional, but technical difficulties are mandatory.",
+        textAlign: TextAlign.center,
+        style: GoogleFonts.ubuntu(
+          color: Colors.white,
+          fontSize: MediaQuery.of(context).size.height / 40,
+          letterSpacing: 0,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    ),
+    Container(
+      padding:
+          EdgeInsets.symmetric(vertical: 0, horizontal: MediaQuery.of(context).size.width / 90),
+      child: Text(
+        "Sirf 2 minutes hai tumhare pass",
+        textAlign: TextAlign.center,
+        style: GoogleFonts.ubuntu(
+          color: Colors.white,
+          fontSize: MediaQuery.of(context).size.height / 40,
+          letterSpacing: 0,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    ),
+    Container(
+      padding:
+          EdgeInsets.symmetric(vertical: 0, horizontal: MediaQuery.of(context).size.width / 90),
+      child: Text(
+        "Out of this world trust issues",
+        textAlign: TextAlign.center,
+        style: GoogleFonts.ubuntu(
+          color: Colors.white,
+          fontSize: MediaQuery.of(context).size.height / 40,
+          letterSpacing: 0,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    ),
+    Container(
+      padding:
+          EdgeInsets.symmetric(vertical: 0, horizontal: MediaQuery.of(context).size.width / 90),
+      child: Text(
+        "💿+🔌",
+        textAlign: TextAlign.center,
+        style: GoogleFonts.ubuntu(
+          color: Colors.white,
+          fontSize: MediaQuery.of(context).size.height / 20,
+          letterSpacing: 0,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    ),
+    Container(
+      padding:
+          EdgeInsets.symmetric(vertical: 0, horizontal: MediaQuery.of(context).size.width / 90),
+      child: Text(
+        "🎮+🏬",
+        textAlign: TextAlign.center,
+        style: GoogleFonts.ubuntu(
+          color: Colors.white,
+          fontSize: MediaQuery.of(context).size.height / 20,
+          letterSpacing: 0,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    ),
+    Container(
+      padding:
+          EdgeInsets.symmetric(vertical: 0, horizontal: MediaQuery.of(context).size.width / 90),
+      child: Text(
+        "👓+🛒",
+        textAlign: TextAlign.center,
+        style: GoogleFonts.ubuntu(
+          color: Colors.white,
+          fontSize: MediaQuery.of(context).size.height / 20,
+          letterSpacing: 0,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    ),
+    Container(
+      padding:
+          EdgeInsets.symmetric(vertical: 0, horizontal: MediaQuery.of(context).size.width / 90),
+      child: Text(
+        "🔥✡️🔥",
+        textAlign: TextAlign.center,
+        style: GoogleFonts.ubuntu(
+          color: Colors.white,
+          fontSize: MediaQuery.of(context).size.height / 20,
+          letterSpacing: 0,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    ),
   ];
 }
 
@@ -48,6 +163,14 @@ List<List<String>> answers = [
   ['S', 'P', 'O', 'T', 'I', 'F', 'Y'],
   ['D', 'I', 'G', 'I', 'L', 'O', 'C', 'K', 'E', 'R'],
   ['A', 'M', 'A', 'Z', 'O', 'N'],
+  ['P', 'O', 'K', 'E', 'M', 'O', 'N', 'G', 'O'],
+  ['Z', 'O', 'O', 'M'],
+  ['B', 'E', 'R', 'E', 'A', 'L'],
+  ['A', 'M', 'O', 'N', 'G', 'U', 'S'],
+  ['D', 'I', 'S', 'C', 'O', 'R', 'D'],
+  ['P', 'L', 'A', 'Y', 'S', 'T', 'O', 'R', 'E'],
+  ['L', 'E', 'N', 'S', 'K', 'A', 'R', 'T'],
+  ['B', 'Y', 'J', 'U', 'S'],
 ];
 
 List<Map<String, bool>> answerGuessed = [
@@ -84,6 +207,72 @@ List<Map<String, bool>> answerGuessed = [
     'Z': false,
     'O': false,
     'N': false,
+  },
+  {
+    'P': false,
+    'O': false,
+    'K': false,
+    'E': false,
+    'M': false,
+    'N': false,
+    'G': false,
+  },
+  {
+    'Z': false,
+    'O': false,
+    'M': false,
+  },
+  {
+    'B': false,
+    'E': false,
+    'R': false,
+    'A': false,
+    'L': false,
+  },
+  {
+    'A': false,
+    'M': false,
+    'O': false,
+    'N': false,
+    'G': false,
+    'U': false,
+    'S': false,
+  },
+  {
+    'D': false,
+    'I': false,
+    'S': false,
+    'C': false,
+    'O': false,
+    'R': false,
+  },
+  {
+    'P': false,
+    'L': false,
+    'A': false,
+    'Y': false,
+    'S': false,
+    'T': false,
+    'O': false,
+    'R': false,
+    'E': false,
+  },
+  {
+    'L': false,
+    'E': false,
+    'N': false,
+    'S': false,
+    'K': false,
+    'A': false,
+    'R': false,
+    'T': false,
+  },
+  {
+    'B': false,
+    'Y': false,
+    'J': false,
+    'U': false,
+    'S': false,
   },
 ];
 
